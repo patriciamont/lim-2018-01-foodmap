6bf33668dfe58453673a4df42d7bc41269ed1df0
patriciamont <fiorellamonteverde@gmail.com>
2018-08-08 05:46:42 -0500

    eliminando archivos para la api de google y añadiendo carpeta data con un json

--- js/wall.js
+++ js/wall.js
@@ -1,10 +1,38 @@
+const btnSpecialty=document.getElementById('specialty');
+const btnEthnic=document.getElementById('ethnic');
+const btnBudget=document.getElementById('budget');
+
+
+
+const listSpecialty=()=>{
+fetch('../data/restaurants.json')
+.then(response=>
+response.json()).then(result=>{
+    console.log(result)
+})
+
+}
+
+
+
+
+btnSpecialty.addEventListener('click',()=>{
+    listSpecialty();
+})
+
+
+
+
+
+
+
 /* function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
       });
 } */
-
+/* 
 function initMap(){
     const ubicacion=new Localizacion(()=>{
         const options={
@@ -17,4 +45,6 @@ function initMap(){
         var map=document.getElementById('map');
         const mapa=new google.maps.Map(map,options);
     });
-}  
+}   */
+
+
